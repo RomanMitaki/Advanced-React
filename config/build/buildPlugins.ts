d200074c08d06fea5177fc3e02360f2c5af4695e
RomanMitaki <r.mitaki@yandex.ru>
2025-02-18 21:39:34 +0300

    Part 3, Class 6. Обновить плагины сборки и добавить CI workflow
    Обновлен способ подключения ReactRefreshWebpackPlugin для повышения читаемости и добавлена проверка режима разработки для плагинов. В package.json добавлен скрипт для UI тестирования в CI. Также создан и настроен GitHub Actions workflow для linting, тестирования и сборки проекта.

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,26 +1,33 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import { ReactRefreshPlugin } from '@pmmmwh/react-refresh-webpack-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import webpack from 'webpack';
 
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
-    return [
-        new HtmlWebpackPlugin({ template: paths.html }),
+    const plugins = [
+        new HtmlWebpackPlugin({
+            template: paths.html
+        }),
         new webpack.ProgressPlugin(),
         new MiniCssExtractPlugin({
             filename: 'css/[name].[contenthash:8].css',
-            chunkFilename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css'
         }),
         new webpack.DefinePlugin({
-            __IS_DEV__: JSON.stringify(isDev),
+            __IS_DEV__: JSON.stringify(isDev)
         }),
-        new webpack.HotModuleReplacementPlugin(),
-        new ReactRefreshWebpackPlugin(),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false,
-        }),
-    ];
+        new ReactRefreshPlugin()
+    ]
+
+    if (isDev) {
+        plugins.push(new webpack.HotModuleReplacementPlugin())
+        plugins.push(new BundleAnalyzerPlugin({
+            openAnalyzer: false
+        }))
+    }
+
+    return plugins
 }
