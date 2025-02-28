c1cf3e4fee4317597eec37aa6ba9c3793ec0429b
RomanMitaki <r.mitaki@yandex.ru>
2025-02-28 14:39:49 +0300

    Улучшить типизацию параметров в ReduxDecorator
    Изменены параметры `initialState` и `asyncReducers` на необязательные. Это повышает гибкость функции и упрощает её использование в различных сценариях.

--- src/shared/config/storybook/ReduxDecorator/ReduxDecorator.tsx
+++ src/shared/config/storybook/ReduxDecorator/ReduxDecorator.tsx
@@ -5,8 +5,8 @@ import { DeepPartial } from '@/app/types/general';
 import { createReduxStore, StateSchema } from '@/app/providers/StoreProvider';
 
 export const ReduxDecorator = (
-    initialState: DeepPartial<StateSchema>,
-    asyncReducers: ReducersMapObject<StateSchema>,
+    initialState?: DeepPartial<StateSchema>,
+    asyncReducers?: ReducersMapObject<StateSchema>,
 ) => (Story: any) => {
     const store = createReduxStore(initialState as StateSchema, asyncReducers);
     return (
